3634014f1919234fbb5ad637103a2f3d8d0f4811
Lee Haewon <hawon2205@ewhain.net>
2024-02-04 20:54:27 +0900

    refactor: 응답 형식 통일 (#18)

--- src/main/java/ewha/lux/once/domain/user/controller/UserController.java
+++ src/main/java/ewha/lux/once/domain/user/controller/UserController.java
@@ -4,16 +4,17 @@ import ewha.lux.once.domain.user.dto.LoginResponseDto;
 import ewha.lux.once.domain.user.dto.SignInRequestDto;
 import ewha.lux.once.domain.user.dto.SignupRequestDto;
 import ewha.lux.once.domain.user.dto.postSearchCardListRequestDto;
-import ewha.lux.once.global.common.UserAccount;
 import ewha.lux.once.domain.user.entity.Users;
 import ewha.lux.once.domain.user.service.UserService;
-import ewha.lux.once.global.common.ResponseDto;
+import ewha.lux.once.global.common.CommonResponse;
+import ewha.lux.once.global.common.CustomException;
+import ewha.lux.once.global.common.ResponseCode;
+import ewha.lux.once.global.common.UserAccount;
 import ewha.lux.once.global.security.JwtProvider;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -27,69 +28,78 @@ import java.text.ParseException;
 @RequiredArgsConstructor
 @RequestMapping("user")
 public class UserController {
+
     private final UserService userService;
     private final JwtProvider jwtProvider;
 
-    @PostMapping("/signup") // 회원가입
-    public ResponseEntity<ResponseDto<Object>> signup(@RequestBody SignupRequestDto request) throws ParseException {
-        try{
+    // [Post] 회원가입
+    @PostMapping("/signup")
+    public CommonResponse<?> signup(@RequestBody SignupRequestDto request) throws ParseException {
+        try {
             Users users = userService.signup(request);
 
             String accessToken = jwtProvider.generateAccessToken(users.getLoginId());
             String refreshToken = jwtProvider.generateRefreshToken(users.getLoginId());
 
-            LoginResponseDto loginResponseDto = new LoginResponseDto(users.getId(),accessToken,refreshToken);
-            return ResponseEntity.ok(ResponseDto.response(1000,true, "요청에 성공하였습니다.",loginResponseDto));
-        } catch (Exception e){
-            return ResponseEntity.ok(ResponseDto.response(2000,false, "요청에 성공하였습니다."));
+            LoginResponseDto loginResponseDto = new LoginResponseDto(users.getId(), accessToken, refreshToken);
+            return new CommonResponse<>(ResponseCode.SUCCESS, loginResponseDto);
+        } catch (Exception e) {
+            return new CommonResponse<>(ResponseCode.SUCCESS); // ** 예외 처리 필요 **
         }
     }
 
-    @PostMapping("/login") // 로그인
-    public ResponseEntity<ResponseDto<Object>> signin(@RequestBody SignInRequestDto request) {
-        try{
+    // [Post] 로그인
+    @PostMapping("/login")
+    public CommonResponse<?> signin(@RequestBody SignInRequestDto request) {
+        try {
             Users user = userService.authenticate(request);
 
             String accessToken = jwtProvider.generateAccessToken(user.getLoginId());
             String refreshToken = jwtProvider.generateRefreshToken(user.getLoginId());
 
-            LoginResponseDto loginResponseDto = new LoginResponseDto(user.getId(),accessToken,refreshToken);
-            return ResponseEntity.ok(ResponseDto.response(1000,true, "요청에 성공하였습니다.",loginResponseDto));
-        } catch (Exception e){
-            return ResponseEntity.ok(ResponseDto.response(2000,false, "요청에 실패하였습니다."));
+            LoginResponseDto loginResponseDto = new LoginResponseDto(user.getId(), accessToken, refreshToken);
+
+            return new CommonResponse<>(ResponseCode.SUCCESS, loginResponseDto);
+        } catch (CustomException e) {
+            return new CommonResponse<>(e.getStatus());
         }
     }
 
-    @DeleteMapping ("/quit")
+    // [Delete] 회원 탈퇴
+    @DeleteMapping("/quit")
     @ResponseBody
-    public ResponseEntity<ResponseDto<Object>> quitUsers (@AuthenticationPrincipal UserAccount userAccount) {
+    public CommonResponse<?> quitUsers(@AuthenticationPrincipal UserAccount userAccount) {
         userService.deleteUsers(userAccount.getUsers());
-        return ResponseEntity.ok(ResponseDto.response(1000,true,"요청에 성공하였습니다."));
+        return new CommonResponse<>(ResponseCode.SUCCESS);
     }
 
+    // [Get] 회원 정보 조회
     @GetMapping("/edit")
     @ResponseBody
-    public ResponseEntity<ResponseDto<Object>> userEdit (@AuthenticationPrincipal UserAccount userAccount) {
-        return ResponseEntity.ok(ResponseDto.response(1000,true,"요청에 성공하였습니다.",userService.getUserEdit(userAccount.getUsers())));
+    public CommonResponse<?> userEdit(@AuthenticationPrincipal UserAccount userAccount) {
+        return new CommonResponse<>(ResponseCode.SUCCESS, userService.getUserEdit(userAccount.getUsers()));
     }
 
+    // [Get] 카드사별 카드 검색
     @GetMapping("/card/search")
     @ResponseBody
-    public ResponseEntity<ResponseDto<Object>> searchCard (@Param("code")String code) {
-        return ResponseEntity.ok(ResponseDto.response(1000,true,"요청에 성공하였습니다.",userService.getSearchCard(code)));
+    public CommonResponse<?> searchCard(@Param("code") String code) {
+        return new CommonResponse<>(ResponseCode.SUCCESS, userService.getSearchCard(code));
     }
 
+    // [Post] 카드 등록
     @PostMapping("/card")
     @ResponseBody
-    public ResponseEntity<ResponseDto<Object>> postSearchCard (@AuthenticationPrincipal UserAccount userAccount,@RequestBody postSearchCardListRequestDto request) {
-        userService.postSearchCard(userAccount.getUsers(),request);
-        return ResponseEntity.ok(ResponseDto.response(1000,true,"요청에 성공하였습니다."));
+    public CommonResponse<?> postSearchCard(@AuthenticationPrincipal UserAccount userAccount, @RequestBody postSearchCardListRequestDto request) {
+        userService.postSearchCard(userAccount.getUsers(), request);
+        return new CommonResponse<>(ResponseCode.SUCCESS);
     }
 
-    @PatchMapping(value="/edit/profile",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    // [Patch] 프로필 등록
+    @PatchMapping(value = "/edit/profile", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @ResponseBody
-    public ResponseEntity<ResponseDto<Object>> editProfile (@AuthenticationPrincipal UserAccount userAccount, HttpServletRequest request, @RequestParam(value="userProfileImg") MultipartFile userProfileImg) throws IOException {
-        return ResponseEntity.ok(ResponseDto.response(1000,true,"요청에 성공하였습니다.",userService.patchEditProfile(userAccount.getUsers(),userProfileImg)));
+    public CommonResponse<?> editProfile(@AuthenticationPrincipal UserAccount userAccount, HttpServletRequest request, @RequestParam(value = "userProfileImg") MultipartFile userProfileImg) throws IOException {
+        return new CommonResponse<>(ResponseCode.SUCCESS, userService.patchEditProfile(userAccount.getUsers(), userProfileImg));
     }
 }
 
