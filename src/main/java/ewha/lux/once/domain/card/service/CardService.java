2a701f0d84a49d237eba3c02c0e1c009acfd1b4d
Lee Haewon <hawon2205@ewhain.net>
2024-03-23 21:31:15 +0900

    feat: 카드 혜택 요약 자동화 구현 (#57)

--- src/main/java/ewha/lux/once/domain/card/service/CardService.java
+++ src/main/java/ewha/lux/once/domain/card/service/CardService.java
@@ -1,37 +1,43 @@
 package ewha.lux.once.domain.card.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import ewha.lux.once.domain.card.dto.*;
+import ewha.lux.once.domain.card.entity.BenefitSummary;
 import ewha.lux.once.domain.card.entity.Card;
 import ewha.lux.once.domain.card.entity.OwnedCard;
-import ewha.lux.once.domain.home.entity.Announcement;
+import ewha.lux.once.domain.home.dto.BenefitDto;
 import ewha.lux.once.domain.home.entity.ChatHistory;
+import ewha.lux.once.domain.home.service.CODEFAPIService;
+import ewha.lux.once.domain.home.service.CODEFAsyncService;
+import ewha.lux.once.domain.home.service.OpenaiService;
 import ewha.lux.once.domain.user.entity.Users;
 import ewha.lux.once.global.common.CustomException;
 import ewha.lux.once.global.common.ResponseCode;
-import ewha.lux.once.global.repository.CardRepository;
-import ewha.lux.once.global.repository.ChatHistoryRepository;
-import ewha.lux.once.global.repository.OwnedCardRepository;
-import ewha.lux.once.global.repository.UsersRepository;
+import ewha.lux.once.global.repository.*;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import ewha.lux.once.domain.home.service.CODEFAPIService;
-import ewha.lux.once.domain.home.service.CODEFAsyncService;
 
 import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class CardService {
 
     private final OwnedCardRepository ownedCardRepository;
     private final ChatHistoryRepository chatHistoryRepository;
     private final UsersRepository usersRepository;
+    private final CardRepository cardRepository;
+    private final BenefitSummaryRepository benefitSummaryRepository;
+
     private final CODEFAPIService codefapi;
     private final CODEFAsyncService codefAsyncService;
-    private final CardRepository cardRepository;
+    private final OpenaiService openaiService;
 
     public MyWalletResponseDto.MyWalletProfileDto getMyWalletInfo(Users nowUser) throws CustomException {
         List<OwnedCard> ownedCards = ownedCardRepository.findOwnedCardByUsers(nowUser);
@@ -42,8 +48,11 @@ public class CardService {
 
         List<MyWalletResponseDto.OwnedCardListDto> ownedCardList = ownedCards.stream()
                 .map(ownedCard -> {
-                    String cardSummary = ownedCard.getCard().getBenefitSummary();
-                    List<MyWalletResponseDto.CardBenefitListDto> cardBenefitList = splitCardSummary(cardSummary);
+                    List<BenefitSummary> cardSummary = benefitSummaryRepository.findByCard(ownedCard.getCard());
+                    List<MyWalletResponseDto.CardBenefitListDto> cardBenefitList = new ArrayList<>();
+                    for (BenefitSummary summary : cardSummary) {
+                        cardBenefitList.add(new MyWalletResponseDto.CardBenefitListDto(summary.getBenefitField(), summary.getBenefitContents()));
+                    }
                     return new MyWalletResponseDto.OwnedCardListDto(
                             ownedCard.getId(),
                             ownedCard.getCard().getName(),
@@ -52,7 +61,7 @@ public class CardService {
                             ownedCard.isMain(),
                             ownedCard.getPerformanceCondition(),
                             ownedCard.getCurrentPerformance(),
-                            Math.max(ownedCard.getPerformanceCondition() - ownedCard.getCurrentPerformance(),0),
+                            Math.max(ownedCard.getPerformanceCondition() - ownedCard.getCurrentPerformance(), 0),
                             cardBenefitList
                     );
                 })
@@ -65,7 +74,7 @@ public class CardService {
 
     public void postCardPerformance(Users nowUser, CardPerformanceRequestDto cardPerformanceRequestDto) throws CustomException {
         OwnedCard ownedCard = ownedCardRepository.findOwnedCardByCardIdAndUsers(cardPerformanceRequestDto.getOwnedCardId(), nowUser);
-        if(ownedCard != null) {
+        if (ownedCard != null) {
             ownedCard.setPerformanceCondition(cardPerformanceRequestDto.getPerformanceCondition());
             ownedCardRepository.save(ownedCard);
         } else {
@@ -76,7 +85,7 @@ public class CardService {
 
     public MontlyBenefitResponseDto.MontlyBenefitProfileDto getMontlyBenefitInfo(Users nowUser, int month) throws CustomException {
         List<ChatHistory> chatHistories = chatHistoryRepository.findByUsers(nowUser);
-        if(chatHistories.isEmpty()){
+        if (chatHistories.isEmpty()) {
             new CustomException(ResponseCode.CHAT_HISTORY_NOT_FOUND);
         }
 
@@ -105,8 +114,8 @@ public class CardService {
         List<MontlyBenefitResponseDto.BenefitListDto> benefitList = categories.stream()
                 .map(category -> new MontlyBenefitResponseDto.BenefitListDto(
                         category,
-                        categoryGetDiscount.getOrDefault(category,0),
-                        categoryGetPaymentAmount.getOrDefault(category,0)
+                        categoryGetDiscount.getOrDefault(category, 0),
+                        categoryGetPaymentAmount.getOrDefault(category, 0)
                 ))
                 .collect(Collectors.toList());
 
@@ -138,15 +147,16 @@ public class CardService {
         }
         return cardBenefitList;
     }
-    public List<CodefCardListResponseDto> getCodefCardList(Users nowUser,CodefCardListRequestDto codefCardListRequestDto) throws CustomException {
+
+    public List<CodefCardListResponseDto> getCodefCardList(Users nowUser, CodefCardListRequestDto codefCardListRequestDto) throws CustomException {
         // 커넥티드 아이디
         String connectedId;
-        if(nowUser.getConnectedId() == null){  // 저장되어있지 않은 경우
+        if (nowUser.getConnectedId() == null) {  // 저장되어있지 않은 경우
             // 계정 생성
             connectedId = codefapi.CreateConnectedID(codefCardListRequestDto);
             nowUser.setConnectedId(connectedId);
             usersRepository.save(nowUser);
-        } else if (codefapi.IsRegistered(codefCardListRequestDto.getCode(),nowUser.getConnectedId())=="0"){  // 커넥티드 아이디가 해당 카드사와 연결되어있지 않은 경우
+        } else if (codefapi.IsRegistered(codefCardListRequestDto.getCode(), nowUser.getConnectedId()) == "0") {  // 커넥티드 아이디가 해당 카드사와 연결되어있지 않은 경우
             // 계정 추가
             connectedId = codefapi.AddToConnectedID(nowUser, codefCardListRequestDto);
         } else {
@@ -166,7 +176,8 @@ public class CardService {
         return cardDTOList;
 
     }
-    public void postRegisterCard(Users nowUser, MainCardRequestDto mainCardRequestDto) throws CustomException{
+
+    public void postRegisterCard(Users nowUser, MainCardRequestDto mainCardRequestDto) throws CustomException {
         // 카드
         Optional<Card> optionalCard = cardRepository.findCardByName(mainCardRequestDto.getCardName());
         Card card = optionalCard.orElseThrow(() -> new CustomException(ResponseCode.CARD_NOT_FOUND));
@@ -185,12 +196,12 @@ public class CardService {
         String connectedId = nowUser.getConnectedId();
 
         // 실적 조회
-        HashMap<String,Object> performResult = codefapi.Performace(mainCardRequestDto.getCode(),connectedId,mainCardRequestDto.getCardName());
+        HashMap<String, Object> performResult = codefapi.Performace(mainCardRequestDto.getCode(), connectedId, mainCardRequestDto.getCardName());
         int performanceCondition = (int) performResult.get("performanceCondition");
         int currentPerformance = (int) performResult.get("currentPerformance");
         String cardNo = (String) performResult.get("resCardNo");
 
-        codefAsyncService.saveFavorite(mainCardRequestDto.getCode(),connectedId, ownedCard,nowUser,cardNo);
+        codefAsyncService.saveFavorite(mainCardRequestDto.getCode(), connectedId, ownedCard, nowUser, cardNo);
 
         ownedCard.setMaincard();
         ownedCard.setPerformanceCondition(performanceCondition);
@@ -198,9 +209,38 @@ public class CardService {
 
         ownedCardRepository.save(ownedCard);
     }
+
     // 보유 주카드 실적 업데이트
     public void updateOwnedCardsPerformance(Users nowUser) throws CustomException {
         codefAsyncService.updateOwnedCardsPerformanceCodef(nowUser);
     }
 
+    // 매주 월요일 04:00 AM 카드 혜택 정보 요약 작업
+    @Scheduled(cron = "0 0 4 ? * 1")
+    public void updateBenefitSummary() throws CustomException, JsonProcessingException {
+
+        List<Card> cardList = cardRepository.findAll();
+
+        int index = 1;
+        for (Card card : cardList) {
+            // 기존의 BenefitSummary 삭제
+            List<BenefitSummary> existingSummaries = benefitSummaryRepository.findByCard(card);
+            benefitSummaryRepository.deleteAll(existingSummaries);
+
+            log.info("[" + card.getName() + "] - 카드 혜택 요약 중... (" + index + "/" + cardList.size() + ")");
+            BenefitDto[] benefitJson = openaiService.gptBenefitSummary(card.getBenefits());
+
+            for (BenefitDto benefit : benefitJson) {
+                BenefitSummary benefitSummary = BenefitSummary.builder()
+                        .benefitField(benefit.getBenefit_field())
+                        .benefitContents(benefit.getContent())
+                        .card(card)
+                        .build();
+
+                benefitSummaryRepository.save(benefitSummary);
+            }
+            index++;
+        }
+        log.info("전체 카드 혜택 요약 완료");
+    }
 }
